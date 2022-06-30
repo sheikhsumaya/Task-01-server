df957e5c3ab3b136071b4948ead0db10261ec571
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-07-01 05:00:20 +0600

    final

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//oRbNMguD5e6wsQHv 
-//to_do_list
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w3c6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
